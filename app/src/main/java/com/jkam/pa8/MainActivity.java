3f9e8f2e85d4239160db4b112afd0396a5816cda
Jakob Kubicki <jkubicki@zagmail.gonzaga.edu>
2021-12-08 12:59:14 -0800

    Second Commit

--- app/src/main/java/com/jkam/pa8/MainActivity.java
+++ app/src/main/java/com/jkam/pa8/MainActivity.java
@@ -1,14 +1,86 @@
 package com.jkam.pa8;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = "MainActivityTag";
+    List<String> results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        results = new ArrayList<>();
+        results.add("One");
+        results.add("Two");
+        results.add("Three");
+        results.add("Four");
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        CustomAdapter adapter = new CustomAdapter();
+        recyclerView.setAdapter(adapter);
+
+
+        EditText searchEditText = (EditText) findViewById(R.id.editTextSearch);
+
+        searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEARCH) { // so the keyboard action button is a search icon
+                    // call your search method here 
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
+        class CustomViewHolder extends RecyclerView.ViewHolder {
+            TextView text1;
+            public CustomViewHolder(@NonNull View itemView) {
+                super(itemView);
+                text1 = itemView.findViewById(android.R.id.text1);
+            }
+
+            public void updateView(String s) {
+                text1.setText(s);
+            }
+        }
+
+        @NonNull
+        @Override
+        public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(MainActivity.this)
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new CustomViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+            String b = results.get(position);
+            holder.updateView(b);
+        }
+
+        @Override
+        public int getItemCount() {
+            return results.size();
+        }
     }
 }
